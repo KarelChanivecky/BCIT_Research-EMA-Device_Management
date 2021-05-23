36ee91c15070b0cc1d69567aa618b584da2f830b
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-05-22 20:13:33 -0700

    Implemented MenuProvider

--- src/ca/bcit_research/ema/MenuFSM/MenuProvider.java
+++ src/ca/bcit_research/ema/MenuFSM/MenuProvider.java
@@ -1,5 +1,7 @@
 package ca.bcit_research.ema.MenuFSM;
 
+import java.util.ArrayList;
+
 /**
  * A MenuProvider is a source of menu options.
  *
@@ -7,4 +9,26 @@ package ca.bcit_research.ema.MenuFSM;
  * @version May, 2021
  */
 public interface MenuProvider {
+
+    /**
+     * Gets the options provided by this MenuProvider
+     * @return a list of Option
+     */
+    ArrayList<Option> getOptions();
+
+    /**
+     * Select and run an option.
+     *
+     * @param option the chosen option
+     * @return The next MenuProvider to present
+     */
+    MenuProvider select(Option option);
+
+    /**
+     * Presents this MenuProvider as an Option.
+     *
+     * @param position The position that this option will take
+     * @return An Option to navigate into this MenuProvider
+     */
+    Option toOption(int position);
 }
