8b7e1693a451bbd46b30fb484d565656b54a49f2
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-05-23 22:56:42 -0700

    Renamed and implemented FileDirectoryMenu. Removed ActionMenu

---
+++ src/ca/bcit_research/ema/MenuFSM/FileDirectoryMenu.java
@@ -0,0 +1,32 @@
+package ca.bcit_research.ema.MenuFSM;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * A FileMenu is a menu that acts on the filesystem.
+ *
+ * @author Karel Chanivecky Garcia
+ * @version May, 2021
+ */
+public abstract class FileDirectoryMenu extends NavigationMenu {
+    protected final String workingDirectoryPath;
+
+    public FileDirectoryMenu(String name, String workingDirectoryPath, MenuProvider parentMenu)
+            throws IllegalArgumentException, FileNotFoundException {
+        super(name, parentMenu);
+
+        File dir = new File(workingDirectoryPath);
+
+        if (!dir.exists()) {
+            throw new FileNotFoundException("Invalid path");
+        }
+
+        if (!dir.isDirectory()) {
+            throw new NotDirectoryPathException("You must enter a path");
+        }
+
+        this.workingDirectoryPath = workingDirectoryPath;
+    }
+
+}
