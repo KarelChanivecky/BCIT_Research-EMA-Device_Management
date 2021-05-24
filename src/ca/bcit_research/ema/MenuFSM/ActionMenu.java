9d1da9c9231cc8d12547f5d1e469177a7fec9292
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-05-23 22:06:47 -0700

    Implemented a base ActionMenu

--- src/ca/bcit_research/ema/MenuFSM/ActionMenu.java
+++ src/ca/bcit_research/ema/MenuFSM/ActionMenu.java
@@ -4,8 +4,15 @@ package ca.bcit_research.ema.MenuFSM;
 /**
  * An ActionMenu is a menu that provides actions that do not depend on other state of the application.
  *
+ * Extend this class if you want a Menu provider with hardcoded behaviour
+ *
  * @author Karel Chanivecky Garcia
  * @version May, 2021
  */
-public class ActionMenu extends NavigationMenu {
+public class ActionMenu extends BaseMenu {
+
+    public ActionMenu(String name) throws IllegalArgumentException {
+        super(name);
+    }
+
 }
