b08218ff0840d7c18e0b657825167665a963a10b
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-05-24 16:27:09 -0700

    Implemented OptionSerializer

---
+++ src/ca/bcit_research/ema/MenuFSM/Model/OptionSerializer.java
@@ -0,0 +1,21 @@
+package ca.bcit_research.ema.MenuFSM.Model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Presents the options of all given MenuProviders as one list.
+ *
+ * @author Karel Chanivecky Garcia
+ * @version May, 2021
+ */
+public class OptionSerializer implements MenuSerializer{
+    @Override
+    public ArrayList<Option> getOptions(List<MenuProvider> menuProviders, MenuProvider... otherMenus) {
+        ArrayList<Option> options = new ArrayList<>();
+        menuProviders.forEach(menuProvider -> options.addAll(menuProvider.getOptions()));
+        Arrays.asList(otherMenus).forEach(menuProvider -> options.addAll(menuProvider.getOptions()));
+        return options;
+    }
+}
