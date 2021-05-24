8f8a1eb6c59137c9b4f01cdc2fc3a9a17219e615
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-05-24 16:02:33 -0700

    Implamented MockStringMenu

---
+++ src/ca/bcit_research/ema/MenuFSM/MockStringMenu.java
@@ -0,0 +1,22 @@
+package ca.bcit_research.ema.MenuFSM;
+
+
+import ca.bcit_research.ema.MenuFSM.Model.BaseMenuProvider;
+import ca.bcit_research.ema.MenuFSM.Model.Option;
+
+/**
+ * Used to test functionality of MenuProvider FSM.
+ *
+ * @author Karel Chanivecky Garcia
+ * @version May, 2021
+ */
+public class MockStringMenu extends BaseMenuProvider {
+
+    MockStringMenu(String name, String...optionNames) throws IllegalArgumentException {
+        super(name);
+        for (String optionName: optionNames) {
+            options.add(new Option(optionName, () -> this));
+        }
+    }
+
+}
