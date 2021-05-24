786ae35faa0752f8ef43598052e5bc8411d690f6
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-05-23 22:49:10 -0700

    Implemented DeviceMenu. Made DeviceMenu abstract

--- src/ca/bcit_research/ema/MenuFSM/DeviceMenu.java
+++ src/ca/bcit_research/ema/MenuFSM/DeviceMenu.java
@@ -1,10 +1,19 @@
 package ca.bcit_research.ema.MenuFSM;
 
+import ca.bcit_research.ema.HardwareModeling.Device;
+
 /**
  * A DeviceMenu is a menu that acts on the virtual model of a Device.
  *
  * @author Karel Chanivecky Garcia
  * @version May, 2021
  */
-public class DeviceMenu extends NavigationMenu{
+public abstract class DeviceMenu extends NavigationMenu {
+    private final Device device;
+
+    public DeviceMenu(String name, Device device, MenuProvider parentMenu) throws IllegalArgumentException {
+        super(name, parentMenu);
+        this.device = device;
+    }
+
 }
