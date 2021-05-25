2127806fcf31be00a78a5a939fed5182712b8ec1
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-05-24 23:58:54 -0700

    Renamed OtherConfig OtherProperty

--- src/main/java/ca/bcit_research/ema/HardwareModeling/OtherProperty.java
+++ src/main/java/ca/bcit_research/ema/HardwareModeling/OtherProperty.java
@@ -1,16 +1,21 @@
 package ca.bcit_research.ema.HardwareModeling;
 
+import ca.bcit_research.ema.MenuFSM.Model.BaseMenuProvider;
+import ca.bcit_research.ema.MenuFSM.Model.MenuProvider;
+import ca.bcit_research.ema.MenuFSM.Model.NavigationMenuProvider;
+import ca.bcit_research.ema.MenuFSM.Model.Option;
+
 /**
  * This class models a Key value pair of configurations.
  *
  * @author Karel Chanivecky Garcia
  * @version May, 2021
  */
-public class OtherConfig implements KeyValue<String, String>{
+public class OtherProperty implements KeyValue<String, String> {
         String name;
         String value;
 
-    public OtherConfig(String name, String value) {
+    public OtherProperty(String name, String value) {
         if (name.length() == 0) {
             throw new IllegalArgumentException("Metric name must be at least 1 character long");
         }
@@ -32,5 +37,13 @@ public class OtherConfig implements KeyValue<String, String>{
     public boolean setValue(String newValue) {
         value = newValue;
         return true;
+    }
+
+    @Override
+    public String toString() {
+        return "OtherProperty{" +
+                "name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                '}';
     }
 }
