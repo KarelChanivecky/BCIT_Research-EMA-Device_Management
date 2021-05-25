a67466b9a6511cbac3a2d01696dacca29698b25b
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-05-24 21:01:59 -0700

    Implemented OtherConfig

--- src/main/java/ca/bcit_research/ema/HardwareModeling/OtherConfig.java
+++ src/main/java/ca/bcit_research/ema/HardwareModeling/OtherConfig.java
@@ -1,12 +1,36 @@
 package ca.bcit_research.ema.HardwareModeling;
 
-import ca.bcit_research.ema.MenuFSM.Model.MenuProvider;
-import ca.bcit_research.ema.MenuFSM.Model.Option;
-
-import java.util.Scanner;
-
 /**
+ * This class models a Key value pair of configurations.
+ *
  * @author Karel Chanivecky Garcia
  * @version May, 2021
  */
-public record OtherConfig(String name, String value) {}
+public class OtherConfig implements KeyValue<String, String>{
+        String name;
+        String value;
+
+    public OtherConfig(String name, String value) {
+        if (name.length() == 0) {
+            throw new IllegalArgumentException("Metric name must be at least 1 character long");
+        }
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public String getKey() {
+        return name;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean setValue(String newValue) {
+        value = newValue;
+        return true;
+    }
+}
