49349456f19334fcfbd45e6ffcad466ed19515ad
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-05-27 02:36:28 -0700

    Added PhysicalInterface enum

---
+++ src/main/java/ca/bcit_research/ema/HardwareModeling/PhysicalInterface.java
@@ -0,0 +1,11 @@
+package ca.bcit_research.ema.HardwareModeling;
+
+/**
+ * @author Karel Chanivecky Garcia
+ * @version May, 2021
+ */
+public enum PhysicalInterface {
+    USB,
+    I2C,
+    UART
+}
